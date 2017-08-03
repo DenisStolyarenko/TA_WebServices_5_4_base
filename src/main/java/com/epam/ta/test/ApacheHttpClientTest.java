fae8f98c958f03b94d394da70c0df971f3fd5c8d
Denis Stolyarenko <denis_stolyarenko@epam.com>
2017-08-03 17:09:31 +0600

    Init new project

---
+++ src/main/java/com/epam/ta/test/ApacheHttpClientTest.java
@@ -0,0 +1,63 @@
+package com.epam.ta.test;
+
+import com.epam.ta.application.business_object.User;
+import com.google.gson.Gson;
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ApacheHttpClientTest {
+    @Test
+    public void checkHttpStatus() throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://jsonplaceholder.typicode.com/users");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        int actualStatusCode = response.getStatusLine().getStatusCode();
+        Assert.assertEquals(actualStatusCode, 200,"Response returned NOT 200 status");
+    }
+
+    @Test
+    public void checkHttpResponseHeader() throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://jsonplaceholder.typicode.com/users");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+//        Header[] valuesOfContentTypeHeader = response.getHeaders("content-type");
+//        HeaderIterator it = response.headerIterator("content-type");
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+        String valueOfContentTypeHeader = response.getFirstHeader("content-type").toString();
+        System.out.println(valueOfContentTypeHeader);
+        Assert.assertTrue(valueOfContentTypeHeader.contains("application/json; charset=utf-8"));
+    }
+
+    @Test
+    public void checkResponseBody() throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://jsonplaceholder.typicode.com/users");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        entity.getContent();
+        BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
+//      BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
+//      String output;
+//        while ((output = br.readLine()) != null){
+//            System.out.println(output);
+//        }
+        Gson gson = new Gson();
+        User[] users = gson.fromJson(br, User[].class);
+        System.out.println(users.length);
+        Assert.assertEquals(users.length, 10, "The quantity of user is NOT equal");
+    }
+
+}
---
+++ src/main/java/com/epam/ta/test/RestAssuredTest.java
@@ -0,0 +1,40 @@
+package com.epam.ta.test;
+
+import com.epam.ta.application.business_object.User;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class RestAssuredTest {
+    @BeforeTest
+    public void initTest(){
+        RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void checkHttpStatus(){
+        Response response = given().get("/users").andReturn();
+        int actualStatusCode = response.getStatusCode();
+        System.out.println(actualStatusCode);
+        Assert.assertEquals(actualStatusCode, 200, "Response returned NOT 200 status");
+    }
+
+    @Test
+    public void checkHttpResponseHeader(){
+        Response response = given().get("/users").andReturn();
+        String valueOfContentTypeHeader = response.getHeader("content-type");
+        System.out.println(valueOfContentTypeHeader);
+        Assert.assertTrue(valueOfContentTypeHeader.contains("application/json; charset=utf-8"));
+    }
+
+    @Test
+    public void checkResponseBody(){
+        Response response = given().get("/users").andReturn();
+        User[] users = response.as(User[].class);
+        Assert.assertEquals(users.length, 10, "Count of users is NOT equal 10");
+    }
+}
